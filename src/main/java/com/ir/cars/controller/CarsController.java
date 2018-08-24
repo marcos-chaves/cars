026ab8e9f0f3a536ace2ca492e66fa32448b4bd4
Marcos Chaves <marcos.chaves@ingressorapido.com.br>
2018-08-24 19:17:13 -0300

    Endpoints criados

--- src/main/java/com/ir/cars/controller/CarsController.java
+++ src/main/java/com/ir/cars/controller/CarsController.java
@@ -5,6 +5,7 @@ import com.ir.cars.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,9 +23,27 @@ public class CarsController {
         return "Hello World";
     }
 
-    @RequestMapping(value = "/park", method = RequestMethod.POST, headers = {"Content-type=application/json"})
-    @ResponseStatus(HttpStatus.OK)
-    public void parkCar(@RequestBody Car car) {
-        garage.park(car);
+    @RequestMapping(value = "/register", method = RequestMethod.POST, headers = {"Content-type=application/json"})
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerCar(@RequestBody Car car) {
+        garage.register(car);
+    }
+
+    @RequestMapping(value = "/park/{licensePlate}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void parkCar(@PathVariable("licensePlate") String licensePlate) {
+        garage.park(licensePlate);
+    }
+
+    @RequestMapping(value = "/checkout/{licensePlate}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void checkoutCar(@PathVariable("licensePlate") String licensePlate) {
+        garage.checkout(licensePlate);
+    }
+
+    @RequestMapping(value = "/checkout/{licensePlate}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeCar(@PathVariable("licensePlate") String licensePlate) {
+        garage.remove(licensePlate);
     }
 }
